1fff018ad2221e91da9eec7754cb5fdee267a0f5
YAGHISVAR <143998928+YAGHISVAR@users.noreply.github.com>
2026-04-20 01:25:07 +0530

    Add new user 'yagi' to DEFAULT_USERS

--- js/data.js
+++ js/data.js
@@ -11,6 +11,7 @@
 
 var DEFAULT_USERS = [
   { id:'USR-0001', name:'Master Admin', username:'master', password:'rmi_pinnacle', team:'—', role:'master' },
+   { id:'USR-0002', name:'yagi', username:'yagi', password:'yagi', team:'ROVIO', role:'admin' },
   // ── Add more users here (use the app as Master Admin) ──
   // { id:'USR-0002', name:'Name Here', username:'username', password:'password', team:'ROVIO', role:'member' },
 ];
